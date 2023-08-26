797b34499a4a0184a75c145b0ca62b4688f45c0a
wenh <62581720+bert-wen@users.noreply.github.com>
2023-08-26 18:44:34 +0800

    上传文件

---
+++ src/medium/backtrack/Rotate.java
@@ -0,0 +1,25 @@
+package medium.backtrack;
+
+/**
+ * @projectName: leetcode
+ * @package: medium.backtrack
+ * @className: Rotate (旋转图像）
+ * @author: WenHui
+ * @description: TODO
+ * @date: 2023/2/2 15:36
+ * @version: 1.0
+ */
+public class Rotate {
+    public static void main(String[] args) {
+        int[][] matrix={{1,2,3},{4,5,6},{7,8,9}};
+        rotate(matrix);
+    }
+    public static void rotate(int[][] matrix) {
+        for (int i=0;i<matrix.length;i++){
+            for (int j=0;j<matrix.length;j++){
+                
+            }
+        }
+    }
+
+}
