91be55a70d88e7dec09694c1a42d7f1df211fc91
wenh <2046641767@qq.com>
2023-09-06 15:14:32 +0800

    最深叶节点的最近公共祖先

---
+++ src/main/java/medium/tree/LcaDeepestLeaves.java
@@ -0,0 +1,35 @@
+package medium.tree;
+
+/**
+ * 最深叶节点的最近公共祖先
+ */
+public class LcaDeepestLeaves {
+    static TreeNode ans;
+    static Integer maxDepth=-1;
+    public static void main(String[] args) {
+        int [] arr={3,4,5,6,};
+        TreeNode treeNode = Tree.buildTree(arr);
+        System.out.println(treeNode);
+        TreeNode treeNode1 = lcaDeepestLeaves(treeNode);
+        System.out.println(treeNode1);
+    }
+
+    public static TreeNode lcaDeepestLeaves(TreeNode root) {
+        dfs(root,0);
+        return ans;
+    }
+    public static int dfs(TreeNode root,Integer depth){
+        // 递归结束条件
+        if (root==null){
+            maxDepth=Math.max(depth,maxDepth);
+            return depth;
+        }
+
+        int leftDepthLength=dfs(root.left,depth+1);
+        int rightDepthLength=dfs(root.right,depth+1);
+        if (leftDepthLength==rightDepthLength && leftDepthLength==maxDepth){
+            ans=root;
+        }
+        return Math.max(leftDepthLength,rightDepthLength);
+    }
+}
--- src/main/java/medium/tree/Tree.java
+++ src/main/java/medium/tree/Tree.java
@@ -18,6 +18,23 @@ public class Tree {
         }
         return root;
     }
+    public static TreeNode buildTree(int[] arr) {
+       return createTree(arr,0);
+    }
+    private static TreeNode createTree(int [] arr,int index) {
+        if (index<0 || index>=arr.length){
+            return null;
+        }
+        TreeNode node = new TreeNode(arr[index]);
+        node.left=createTree(arr,2*index+1);
+        node.right=createTree(arr,2*index+2);
+        return node;
+    }
+    /**
+     * 生成二叉搜索树
+     * @param node 数的根节点
+     * @param val 节点的值
+     */
     private static void createBST(TreeNode node, int val) {
         if (val < node.val) {
             if (node.left == null) {
@@ -34,8 +51,8 @@ public class Tree {
     }
 
     public static void main(String[] args) {
-        int [] arr={5,3,2};
-        TreeNode treeNode1 = bulidBST(arr);
+        int [] arr={3,5,1,6};
+        TreeNode treeNode1 = buildTree(arr);
         System.out.println(treeNode1);
     }
 }
